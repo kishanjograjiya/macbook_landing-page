3cb62fc13985f2dd2a3d9fb8bd26bdfe24d618b7
Kishan Jograjiya <kishanjograjiya.dev@gmail.com>
2025-11-19 22:51:30 +0530

    add MacBook model and studio lighting to ProductViewer component

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -2,6 +2,8 @@ import clsx from "clsx";
 import { Canvas } from "@react-three/fiber";
 import { Box, OrbitControls } from "@react-three/drei";
 import useMacBookStore from "../store";
+import MacBookModel14 from "./models/Macbook-14";
+import StudioLights from "./StudioLights";
 
 const ProductViewer = () => {
   const { color, scale, setColor, setScale } = useMacBookStore();
@@ -62,7 +64,8 @@ const ProductViewer = () => {
         id="canvas"
         camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100 }}
       >
-        <Box position={[-1, 1, 0]} scale={10 * scale} material-color={color} />
+        <StudioLights />
+        <MacBookModel14 scale={0.06} position={[0, 0, 0]} />
         <OrbitControls enableZoom={false} />
       </Canvas>
     </section>
